c7aea297edc5ed209f17e64afb1386045bd1173c
Marek Petrik <marek@petrik.us>
2015-05-29 10:27:22 -0400

    Added a simple example testing the sampling-robust behavior

---
+++ examples/test_robust.py
@@ -0,0 +1,33 @@
+import raam
+from raam import robust
+from raam import crobust
+from raam import features
+from raam import examples
+
+import numpy as np
+
+discount = 0.99
+samples = examples.chain.simple_samples(10,discount)
+
+r = crobust.SRoMDP(1, discount)
+
+statenum = lambda x: np.where(x)[0][0]
+zero = lambda x: 0
+
+dict_si = {}
+def sampleindex(x):
+    x = statenum(x)
+    index = dict_si.get(x,0)
+    dict_si[x] = index + 1
+    return index
+    
+
+r.from_samples(samples, decagg_big=statenum, decagg_small=zero,
+                expagg_big=statenum, expagg_small=sampleindex,
+                actagg=features.IdCache())
+                
+r.rmdp.set_uniform_distributions(0.0)
+
+v,_,_,_ = r.rmdp.mpi_jac(1000,stype=robust.SolutionType.Average.value)
+
+print(r.decvalue(10, v))
